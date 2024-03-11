d9730b7755c2251737595efc6efff699083499e7
Murilo Castro <murilo.65@gmail.com>
2024-03-10 21:35:02 -0300

    resolvendo Problemas de tipo no petsItinires

--- src/app/pet-intineraries/page.tsx
+++ src/app/pet-intineraries/page.tsx
@@ -11,23 +11,23 @@ export default function Teste() {
     <section>
       <div className="hidden md:block md:flex gap-6 mt-20 justify-between grow bg-main w-full max-w-full mx-auto dark:bg-zinc-800 text-zinc-900 dark:text-zinc-50">
         <div className='2xl:w-[448px] 2xl:h-full gap-[40px]'>
-          <div className='flex flex-col'>
-            <Form className='flex p-[16px]' />
+          <div className='flex flex-col p-[16px]'>
+            <Form />
           </div>
-          <div>
-            <List className='flex grow'/>
+          <div  className='flex grow' >
+            <List />
           </div>
         </div>
-        <div className='hidden md:block mr-3'>
-          <CarouselServices className='flex grow' />
+        <div className='hidden md:block mr-3 flex grow'>
+          <CarouselServices />
         </div>
       </div>
       <div className="grid grid-cols-1 md:hidden bg-main w-full mx-auto dark:bg-zinc-800 text-zinc-900 dark:text-zinc-50">
         <div>
           <Form />
         </div>
-        <div>
-          <CarouselServices className='flex' />
+        <div className='flex' >
+          <CarouselServices />
         </div>
         <div>
           <List />
